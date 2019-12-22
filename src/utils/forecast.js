396d9627976c86ba46edf419df56d06b7598efc1
dmcgilloway <db_mcgilloway@hotmail.com>
2019-12-22 02:54:13 +0000

    update forecast.js to include todays low/high tempatures

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,8 +2,9 @@ const request = require('request')
 const forecast = (longitute,latitute,callback) => {
 
     const url = 'https://api.darksky.net/forecast/be4b337df011fa9e16af6360300b0707/' +  latitute + ',' +   longitute+ '?units=si&lang=en'
-
+       
         request({ url, json: true }, (error, {body} = {}) => {
+          console.log(body.daily)
           if (error) {
             callback('Unable to connect to weather service!',undefined);
           }
@@ -14,8 +15,8 @@ const forecast = (longitute,latitute,callback) => {
               callback
               (
                   undefined,
-                  body.daily.data[0].summary + " It is currently " + body.currently.temperature +" degrees out. There is " +
-                  (body.currently.precipProbability*100) + "% of rain."
+                  body.daily.data[0].summary + " It is currently " + body.currently.temperature +" degrees out. There is " + (body.currently.precipProbability*100) + "% of rain." + 
+                  'The tempature low for today was: ' + body.daily.data[0].temperatureLow + '. The temperature high today was: ' + body.daily.data[0].temperatureHigh + '.'
               )
           }
         })
